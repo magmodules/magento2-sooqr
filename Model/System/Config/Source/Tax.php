c072b619f775ef16455bf219f7e740afc90a9ae1
Marvin-Magmodules <marvin@magmodules.eu>
2018-01-10 11:29:20 +0100

    1.0.6

---
+++ Model/System/Config/Source/Tax.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright © 2017 Magmodules.eu. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magmodules\Sooqr\Model\System\Config\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+
+/**
+ * Class Tax
+ *
+ * @package Magmodules\Sooqr\Model\System\Config\Source
+ */
+class Tax implements ArrayInterface
+{
+
+    /**
+     * Options array
+     *
+     * @var array
+     */
+    public $options = null;
+
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        if (!$this->options) {
+            $this->options = [
+                ['value' => '', 'label' => __('Default')],
+                ['value' => 'true', 'label' => __('Force Add Tax')],
+            ];
+        }
+        return $this->options;
+    }
+}
